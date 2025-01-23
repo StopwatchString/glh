4895051fca22d218d96910def42aa4a5ba7cee25
Mason Speck <mcs.5299@gmail.com>
2025-01-22 20:51:00 -0500

    Added class block to OpenGLApplication with copy/pastable default appconfig

--- include/glh/classes/OpenGLApplication.h
+++ include/glh/classes/OpenGLApplication.h
@@ -16,6 +16,36 @@
 #include <string>
 #include <functional>
 
+//------------------------------------------------------
+// class: OpenGLApplication
+// description: A small framework for managing a multithreaded
+//              OpenGL window through GLFW with DearImgui
+//              built in and automatically initialized.
+//
+//  Default Config for copy/paste:
+//  OpenGLApplication::ApplicationConfig appConfig;
+//  appConfig.windowName = "";
+//  appConfig.windowInitWidth = 0;
+//  appConfig.windowInitHeight = 0;
+//  appConfig.windowPosX = 0;
+//  appConfig.windowPosY = 0;
+//  appConfig.windowBorderless = false;
+//  appConfig.windowResizeEnable = false;
+//  appConfig.windowDarkmode = false;
+//  appConfig.windowRounded = false;
+//  appConfig.windowAlwaysOnTop = false;
+//  appConfig.vsyncEnable = false;
+//  appConfig.transparentFramebuffer = false;
+//  appConfig.glVersionMajor = 4;
+//  appConfig.glVersionMinor = 6;
+//  appConfig.glslVersionString = "#version 460"; // Used for DearImgui, leave default unless you know what to put here
+//  appConfig.customDrawFunc = nullptr;      // std::function<void(GLFWwindow*)>
+//  appConfig.customKeyCallback = nullptr;   // std::function<void(GLFWwindow* window, int key, int scancode, int action, int mods)>
+//  appConfig.customErrorCallback = nullptr; // std::function<void(int error_code, const char* description)>
+//  appConfig.customDropCallback = nullptr;  // std::function<void(GLFWwindow* window, int count, const char** paths)>
+//  appConfig.customPollingFunc = nullptr;   // std::function<void()>
+//------------------------------------------------------
+
 class OpenGLApplication
 {
 public:
