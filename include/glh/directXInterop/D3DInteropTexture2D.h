fa90059b12f956795502b1b7d3cc9f224c28dd7e
Mason Speck <mcs.5299@gmail.com>
2025-02-09 19:59:52 -0500

    d3dinterop all static now

--- include/glh/directXInterop/D3DInteropTexture2D.h
+++ include/glh/directXInterop/D3DInteropTexture2D.h
@@ -1,3 +1,57 @@
+/*
+ * @file         D3DInteropTexture2D.h
+ * @brief        Static functions for creating shared ID3D11Texture2D
+ *               objects for use with OpenGL.
+ * @creationdate 2025-02-08
+ * @lastupdate   2025-02-08
+ * @license      MIT License (See LICENSE file for details)
+ *
+ * Copyright (c) 2025 Mason Speck
+ *
+ * Description:
+ * A group of static functions and structs to facilitate managing
+ * an ID3D11Texture2D as a mechanism for GPU-level IPC in OpenGL.
+ *
+ * Usage:
+ * Before using these functions, have a valid instance of Direct3DContext.
+ * The definition and handling mechanisms for this are located in
+ * direct3d_utils.h, and this context is used to create and manage
+ * Direct3D resources.
+ *
+ * In the process which will create and fill the texture, create and fill in
+ * a D3DInteropTextureCreateInfo struct. This will be used to generate an
+ * ID3D11Texture2D with the correct parameters. Once this is created, pass
+ * it and the Direct3DContext into createD3DInteropTexture(). This will return
+ * a D3DInteropTexture struct which will contain all necessary handles for
+ * working with this texture as both an OpenGL and Direct3D texture.
+ *
+ * Within the D3DInteropTexture struct, there is a HANDLE
+ * hDxTextureSharedResource. This handle must be shared to the target process
+ * so that it can open the same Direct3D texture resource. NOTE: The target
+ * process must obtain its own copy of the handle, usually through the use
+ * of the DuplicateHandle() function in the Windows API. Simply sharing the
+ * same HANDLE value between processes will not suffice. Typically this is
+ * achieved by sharing the hDxTextureSharedResource and source process id
+ * with the target process, so that it has the tools to create its own handle
+ * via DuplicateHandle().
+ *
+ * Once the target process has obtained the hDxTextureSharedResource, pass it
+ * and its Direct3DContext to openD3DInteropTexture().
+ *
+ * Now both processes have access to the same texture resource, as both an
+ * OpenGL and Direct3D texture. The only stipulation in this interoperability
+ * is that the driver must be given advanced notice of which API you plan to
+ * use before you issue it commands. This is controlled through the interopLock()
+ * and interopUnlock() functions. While 'unlocked' the texture is only
+ * interactable as a Direct3D texture. While 'locked' the texture is only
+ * interactable as an OpenGL texture.
+ *
+ * interopLock() and interopUnlock() **DO NOT** have an impact on inter-process
+ * locking of resources. This is a lock only within the driver to indicate
+ * whether the texture is currently in OpenGL mode. This lock is derived from
+ * OpenGL extensions.
+ */
+
 #ifndef GLH_D3D_INTEROP_TEXTURE2D_H
 #define GLH_D3D_INTEROP_TEXTURE2D_H
 
@@ -7,76 +61,6 @@
 
 #include <D3D11_1.h>
 #include <wrl/client.h> // for Microsoft::WRL::ComPtr template
-
-class D3DInteropTexture2D
-{
-public:
-    D3DInteropTexture2D(GLsizei width, GLsizei height, bool useMipmaps, Direct3DContext context); // Export constructor
-    D3DInteropTexture2D(HANDLE importTextureHandle, Direct3DContext context);                     // Import constructor
-    ~D3DInteropTexture2D();
-
-    D3DInteropTexture2D(const D3DInteropTexture2D& other) = delete;
-    D3DInteropTexture2D& operator=(const D3DInteropTexture2D& other) = delete;
-
-    D3DInteropTexture2D(D3DInteropTexture2D&& other) noexcept = delete;
-    D3DInteropTexture2D& operator=(D3DInteropTexture2D&& other) noexcept = delete;
-
-    // Binds the texture on GL_TEXTURE_2D
-    void bind() const;
-    // Binds '0' on GL_TEXTURE_2D
-    void unbind() const;
-
-    // Upload data into the texture
-    // sourceFormat- The storage format of the data being uploaded (ex: GL_RGBA)
-    // sourceDataType- The data type of the data being uploaded (ex: GL_UNSIGNED_BYTE)
-    // sourceData- Pointer to the data being uploaded
-    void uploadData(GLenum sourceFormat, GLenum sourceDataType, const void* sourceData);
-
-    // Set a parameter on the texture
-    void setParameter(GLenum parameterName, GLint parameter);
-
-    // Get direct OpenGL texture handle
-    GLuint openGLHandle() const { return m_OpenGLTextureName; }
-    ID3D11Texture2D* d3dTexturePointer() const { return m_D3DTexture; }
-    HANDLE d3dTextureSharedResourceHandle() const { return m_hDxTextureSharedResource; }
-    HANDLE wglSharedTextureLockHandle() const { return m_hWglSharedTextureLock; }
-    // Returns true if texture handle != 0
-    bool isValid() const { return m_OpenGLTextureName != 0; }
-    // Returns true if data has been uploaded into texture
-    bool hasData() const { return m_HasData; }
-    // Returns the internal storage format of the texture
-    GLenum internalFormat() const { return m_InternalFormat; }
-    // Returns texture width
-    GLsizei width() const { return m_Width; }
-    // Returns texture height
-    GLsizei height() const { return m_Height; }
-    // Returns number of mipmaps levels
-    GLsizei levels() const { return m_Levels; }
-    // Returns true if texture has mipmaps
-    bool hasMipmaps() const { return m_Levels > 1; }
-
-    // Obtains a lock on the Graphics API interop lock.
-    // The texture is only functional while this lock is active.
-    void interopLock();
-    // Unlocks the Graphics API interop lock
-    void interopUnlock();
-
-private:
-    const Direct3DContext m_Context;
-
-    bool m_HasData{false};
-    GLenum m_InternalFormat{GL_NONE};
-    GLsizei m_Width{0};
-    GLsizei m_Height{0};
-    GLsizei m_Levels{0};
-
-    // Texture handles
-    GLuint m_OpenGLTextureName{0};
-    ID3D11Texture2D* m_D3DTexture{nullptr};
-    HANDLE m_hDxTextureSharedResource{NULL};
-    HANDLE m_hWglSharedTextureLock{NULL};
-    void createSharedTexture();
-};
 
 namespace glh {
 namespace d3dinterop {
@@ -100,12 +84,12 @@ struct D3DInteropTexture
     GLsizei mipmapLevels{0};
 };
 
-D3DInteropTexture createD3DInteropTexture(const D3DInteropTextureCreateInfo& createInfo, const Direct3DContext& context);
+D3DInteropTexture
+    createD3DInteropTexture(const D3DInteropTextureCreateInfo& createInfo, const Direct3DContext& context);
 D3DInteropTexture openD3DInteropTexture(HANDLE dxSharedResourceHandle, const Direct3DContext& context);
 bool destroyD3DInteropTexture(D3DInteropTexture& interopTexture, const Direct3DContext& context);
 void interopLock(D3DInteropTexture& interopTexture, const Direct3DContext& context);
 void interopUnlock(D3DInteropTexture& interopTexture, const Direct3DContext& context);
-
 
 } // namespace d3dinterop
 } // namespace glh
--- src/glh/directXInterop/D3DInteropTexture2D.cpp
+++ src/glh/directXInterop/D3DInteropTexture2D.cpp
@@ -1,3 +1,55 @@
+/**
+ * @file         D3DInteropTexture2D.cpp
+ * @brief        Static functions for creating shared ID3D11Texture2D
+ *               objects for use with OpenGL.
+ * @license      MIT License (See LICENSE file for details)
+ *
+ * Copyright (c) 2025 Mason Speck
+ *
+ * Description:
+ * A group of static functions and structs to facilitate managing
+ * an ID3D11Texture2D as a mechanism for GPU-level IPC in OpenGL.
+ *
+ * Usage:
+ * Before using these functions, have a valid instance of Direct3DContext.
+ * The definition and handling mechanisms for this are located in
+ * direct3d_utils.h, and this context is used to create and manage
+ * Direct3D resources.
+ *
+ * In the process which will create and fill the texture, create and fill in
+ * a D3DInteropTextureCreateInfo struct. This will be used to generate an
+ * ID3D11Texture2D with the correct parameters. Once this is created, pass
+ * it and the Direct3DContext into createD3DInteropTexture(). This will return
+ * a D3DInteropTexture struct which will contain all necessary handles for
+ * working with this texture as both an OpenGL and Direct3D texture.
+ *
+ * Within the D3DInteropTexture struct, there is a HANDLE
+ * hDxTextureSharedResource. This handle must be shared to the target process
+ * so that it can open the same Direct3D texture resource. NOTE: The target
+ * process must obtain its own copy of the handle, usually through the use
+ * of the DuplicateHandle() function in the Windows API. Simply sharing the
+ * same HANDLE value between processes will not suffice. Typically this is
+ * achieved by sharing the hDxTextureSharedResource and source process id
+ * with the target process, so that it has the tools to create its own handle
+ * via DuplicateHandle().
+ *
+ * Once the target process has obtained the hDxTextureSharedResource, pass it
+ * and its Direct3DContext to openD3DInteropTexture().
+ *
+ * Now both processes have access to the same texture resource, as both an
+ * OpenGL and Direct3D texture. The only stipulation in this interoperability
+ * is that the driver must be given advanced notice of which API you plan to
+ * use before you issue it commands. This is controlled through the interopLock()
+ * and interopUnlock() functions. While 'unlocked' the texture is only
+ * interactable as a Direct3D texture. While 'locked' the texture is only
+ * interactable as an OpenGL texture.
+ *
+ * interopLock() and interopUnlock() **DO NOT** have an impact on inter-process
+ * locking of resources. This is a lock only within the driver to indicate
+ * whether the texture is currently in OpenGL mode. This lock is derived from
+ * OpenGL extensions.
+ */
+
 #include "glh/directXInterop/D3DInteropTexture2D.h"
 
 #include "glh/classes/SharedLibraryLoader.h"
@@ -6,248 +58,18 @@
 #include <vector>
 #include <cmath>
 
-//-----------------------------------------------
-// Export Constructor
-//-----------------------------------------------
-D3DInteropTexture2D::D3DInteropTexture2D(GLsizei width, GLsizei height, bool useMipmaps, Direct3DContext context) :
-    m_Width(width),
-    m_Height(height),
-    m_InternalFormat(GL_RGBA8),
-    m_Levels(useMipmaps ? 1 + static_cast<GLsizei>(std::log2(max(width, height))) : 1),
-    m_Context(context)
-{
-    createSharedTexture();
-}
-
-//-----------------------------------------------
-// Import Constructor
-//-----------------------------------------------
-D3DInteropTexture2D::D3DInteropTexture2D(HANDLE importTextureHandle, Direct3DContext context) : m_Context(context)
-{
-    if (importTextureHandle != NULL) {
-        HRESULT hr = context.d3dDevice1->OpenSharedResource1(
-            importTextureHandle, __uuidof(ID3D11Texture2D), reinterpret_cast<void**>(&m_D3DTexture));
-        if (hr != S_OK) {
-            std::cout << "Failed to open imported texture" << std::endl;
-        }
-
-        // Associate the D3DTexture with the generated resource handle
-        if (!wglDXSetResourceShareHandleNV(m_D3DTexture, importTextureHandle)) {
-            std::cerr << "ERROR createSharedTexture() wglDXSetResourceShareHandleNV() failed!" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-
-        // Make the OpenGL Texture
-        glGenTextures(1, &m_OpenGLTextureName);
-
-        // This lock synchronizes control of the texture between D3D and OpenGL
-        // You *must* lock and unlock this resource when using the texture in OpenGL for any operation.
-        // TBD for DirectX operations.
-        m_hWglSharedTextureLock = wglDXRegisterObjectNV(
-            m_Context.hWglD3DDevice, // hDevice  |
-            m_D3DTexture,            // dxObject |
-            m_OpenGLTextureName,     // name     |
-            GL_TEXTURE_2D,           // type     |
-            WGL_ACCESS_READ_WRITE_NV // access   |
-        );
-        if (m_hWglSharedTextureLock == NULL) {
-            std::cerr << "wglDXRegisterObjectNV failed" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-
-        glhTextureParameteri(m_OpenGLTextureName, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glhTextureParameteri(m_OpenGLTextureName, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    }
-}
-
-//-----------------------------------------------
-// Destructor
-//-----------------------------------------------
-D3DInteropTexture2D::~D3DInteropTexture2D()
-{
-    // TODO
-    if (m_Context.hWglD3DDevice != NULL) {
-        interopUnlock();
-    }
-}
-
-//-----------------------------------------------
-// bind()
-//-----------------------------------------------
-void D3DInteropTexture2D::bind() const
-{
-    glhBindTexture(GL_TEXTURE_2D, m_OpenGLTextureName);
-}
-
-//-----------------------------------------------
-// unbind()
-//-----------------------------------------------
-void D3DInteropTexture2D::unbind() const
-{
-    glhBindTexture(GL_TEXTURE_2D, 0);
-}
-
-//-----------------------------------------------
-// uploadData()
-//-----------------------------------------------
-void D3DInteropTexture2D::uploadData(GLenum sourceFormat, GLenum sourceDataType, const void* sourceData)
-{
-    glhTextureSubImage2D(m_OpenGLTextureName, 0, 0, 0, m_Width, m_Height, sourceFormat, sourceDataType, sourceData);
-    if (m_Levels > 1) {
-        glhGenerateTextureMipmap(m_OpenGLTextureName);
-    }
-    m_HasData = true;
-}
-
-//-----------------------------------------------
-// setParameter()
-//-----------------------------------------------
-void D3DInteropTexture2D::setParameter(GLenum parameterName, GLint parameter)
-{
-    glhTextureParameteri(m_OpenGLTextureName, parameterName, parameter);
-}
-
-//-----------------------------------------------
-// interopLock()
-//-----------------------------------------------
-void D3DInteropTexture2D::interopLock()
-{
-    wglDXLockObjectsNV(m_Context.hWglD3DDevice, 1, &m_hWglSharedTextureLock);
-}
-
-//-----------------------------------------------
-// interopUnlock()
-//-----------------------------------------------
-void D3DInteropTexture2D::interopUnlock()
-{
-    wglDXUnlockObjectsNV(m_Context.hWglD3DDevice, 1, &m_hWglSharedTextureLock);
-}
-
-//-----------------------------------------------
-// createSharedTexture()
-//-----------------------------------------------
-void D3DInteropTexture2D::createSharedTexture()
-{
-    bool sRGB = false;
-
-    D3D11_TEXTURE2D_DESC texDesc{};
-    texDesc.Width = m_Width;
-    texDesc.Height = m_Height;
-    texDesc.MipLevels = m_Levels;
-    texDesc.ArraySize = 1;
-    texDesc.Format = sRGB ? DXGI_FORMAT_R8G8B8A8_UNORM_SRGB : DXGI_FORMAT_R8G8B8A8_UNORM;
-    texDesc.SampleDesc.Count = 1;
-    texDesc.SampleDesc.Quality = 0;
-    texDesc.Usage = D3D11_USAGE_DEFAULT;
-    texDesc.BindFlags = D3D11_BIND_RENDER_TARGET;
-    texDesc.CPUAccessFlags = 0;
-    texDesc.MiscFlags = 0;
-    texDesc.MiscFlags = D3D11_RESOURCE_MISC_SHARED_NTHANDLE | D3D11_RESOURCE_MISC_SHARED_KEYEDMUTEX;
-
-    // Initial image data is empty
-    // const D3D11_SUBRESOURCE_DATA* initialData = nullptr;
-
-    // Create initial green color data
-    std::vector<uint8_t> greenColorData(m_Width * m_Height * 4, 0);
-    for (size_t i = 0; i < greenColorData.size(); i += 4) {
-        greenColorData[i] = 0;       // Red
-        greenColorData[i + 1] = 255; // Green
-        greenColorData[i + 2] = 0;   // Blue
-        greenColorData[i + 3] = 255; // Alpha
-    }
-
-    D3D11_SUBRESOURCE_DATA initialData{};
-    initialData.pSysMem = greenColorData.data();
-    initialData.SysMemPitch = m_Width * 4; // RGBA, so 4 bytes per pixel
-    initialData.SysMemSlicePitch = 0;
-
-    HRESULT hr = NULL;
-
-    // Create the DirectX2D texture
-    hr = m_Context.d3dDevice1->CreateTexture2D(
-        &texDesc,     // pDesc        | Texture description object
-        &initialData, // pInitialData | Initial data for texture
-        &m_D3DTexture // ppTexture2D  | Target pointer for created texture
-    );
-    if (hr != S_OK) {
-        std::cerr << "ERROR createSharedTexture() CreateTexture2D() failed!" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    // Get the Resource1 interface from the DirectX2D Texture for CreateSharedHandle()
-    IDXGIResource1* pResource = NULL;
-    hr = m_D3DTexture->QueryInterface(
-        __uuidof(IDXGIResource1), // Interface to retrieve out of COM object (the texture object)
-        (void**)&pResource        // Target to place interface in
-    );
-    if (hr != S_OK) {
-        std::cerr << "ERROR createSharedTexture() QueryInterface failed!" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    // Use CreateSharedHandle() to make a sharable handle betwen OpenGL and DirectX
-    // NOTE: This does not create any sort of association between the texture and shared handle yet.
-    // The IDXGIResource1 interface was retreived from the texture interface, but that does not inherently link them.
-    hr = pResource->CreateSharedHandle(
-        NULL,                                                   // pAttributes
-        DXGI_SHARED_RESOURCE_READ | DXGI_SHARED_RESOURCE_WRITE, // dwAccess
-        NULL,                                                   // lpName
-        &m_hDxTextureSharedResource                             // pHandle
-    );
-    if (hr != S_OK) {
-        std::cerr << "ERROR createSharedTexture() CreateTexture2D() failed!" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    // We don't need the Resource1 interface anymore, so release it (decrementing its ref counter)
-    pResource->Release();
-
-    // Associate the D3DTexture with the generated resource handle
-    if (!wglDXSetResourceShareHandleNV(m_D3DTexture, m_hDxTextureSharedResource)) {
-        std::cerr << "ERROR createSharedTexture() wglDXSetResourceShareHandleNV() failed!" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    // Make the OpenGL Texture
-    glGenTextures(1, &m_OpenGLTextureName);
-
-    // This lock synchronizes control of the texture between D3D and OpenGL
-    // You *must* lock and unlock this resource when using the texture in OpenGL for any operation.
-    // TBD for DirectX operations.
-    m_hWglSharedTextureLock = wglDXRegisterObjectNV(
-        m_Context.hWglD3DDevice, // hDevice  |
-        m_D3DTexture,            // dxObject |
-        m_OpenGLTextureName,     // name     |
-        GL_TEXTURE_2D,           // type     |
-        WGL_ACCESS_READ_WRITE_NV // access   |
-    );
-    if (m_hWglSharedTextureLock == NULL) {
-        std::cerr << "wglDXRegisterObjectNV failed" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    if (m_Levels > 1) {
-        glTextureParameteri(m_OpenGLTextureName, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-        glTextureParameteri(m_OpenGLTextureName, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    }
-    else {
-        glTextureParameteri(m_OpenGLTextureName, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTextureParameteri(m_OpenGLTextureName, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    }
-}
-
 namespace glh {
 namespace d3dinterop {
 
 D3DInteropTexture createD3DInteropTexture(const D3DInteropTextureCreateInfo& createInfo, const Direct3DContext& context)
 {
+    bool sRGB = false;
+
     D3DInteropTexture interopTexture;
     interopTexture.width = createInfo.width;
     interopTexture.height = createInfo.height;
     interopTexture.mipmapLevels
         = createInfo.useMipmaps ? 1 + static_cast<GLsizei>(std::log2(max(createInfo.width, createInfo.height))) : 1;
-
-    bool sRGB = false;
 
     D3D11_TEXTURE2D_DESC texDesc{};
     texDesc.Width = createInfo.width;
@@ -331,8 +153,6 @@ D3DInteropTexture createD3DInteropTexture(const D3DInteropTextureCreateInfo& cre
     glGenTextures(1, &interopTexture.openGLTextureName);
 
     // This lock synchronizes control of the texture between D3D and OpenGL
-    // You *must* lock and unlock this resource when using the texture in OpenGL for any operation.
-    // TBD for DirectX operations.
     interopTexture.hWglSharedTextureLock = wglDXRegisterObjectNV(
         context.hWglD3DDevice,            // hDevice  |
         interopTexture.d3dTexture,        // dxObject |
